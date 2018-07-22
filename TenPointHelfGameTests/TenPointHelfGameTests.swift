d63814fc7e4567943043c6e893bf4054863a450a
5+1 <Masqurin.mig@gmail.com>
2018-07-22 11:42:08 +0800

    測試失敗 以及測試方法不好 準備重寫測試

--- TenPointHelfGameTests/TenPointHelfGameTests.swift
+++ TenPointHelfGameTests/TenPointHelfGameTests.swift
@@ -15,7 +15,8 @@ class TenPointHelfGameTests: XCTestCase {
     
     override func setUp() {
         super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        
+        
     }
     
     override func tearDown() {
@@ -31,13 +32,9 @@ class TenPointHelfGameTests: XCTestCase {
     // 測試 派牌
     func test派牌() {
         
-        //基本上 拿到的牌一定是1~13之間 所以
+        //基本上 拿到的牌一定是1~13之間
         let 牌的範圍 = 1 ... 13
-        
         let 取得牌 = 遊戲規則.派牌()
-        
-        // 糟糕 不知道怎麼檢查數值是否在範圍內
-        // 怎麼測試也忘記了 馬上餵狗 請求支援
         
         let 是否在範圍內: Bool
         switch 取得牌 {
@@ -47,19 +44,18 @@ class TenPointHelfGameTests: XCTestCase {
             是否在範圍內 = false
         }
         XCTAssert(是否在範圍內)
-        //第二次測試 不知道會不會通過測試
-        // 非常好 測試有過 再把這次通過的改變 記錄到git上
     }
     
     // 測試 檢查排是否超過十點半
     func test檢查是否超過十點半() {
         
-        let 派牌 = 12
-        let 現有: Float = 2.5
+        let 派牌 = 1
+        let 現有: Float = 0.5
         let 更新點數 = 遊戲規則.點數轉換(持有點數: 現有, 新得到點數: 派牌)
         let 檢查 = 遊戲規則.檢查點數是否超過十點半(持有點數: 更新點數)
         
-        //測試成功 把這個改變記錄下來
+        //規則二 測試 這種天胡能不能檢查過關
+        // 測試失敗 發現測試寫得不好 改好一點
         
         print("派牌 = ",派牌 ,"\n現有 = ", 現有 ,"\n 更新點數 = ", 更新點數 )
         XCTAssert(!檢查)
