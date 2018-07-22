2e146ab3514725238669f36160bd91b1e3be6670
5+1 <Masqurin.mig@gmail.com>
2018-07-22 10:24:04 +0800

    派牌 測試失敗 「只有一個人 所以直接同步遠端」

---
+++ TenPointHelfGame/GameModel.swift
@@ -0,0 +1,25 @@
+//
+//  GameModel.swift
+//  TenPointHelfGame
+//
+//  Created by 胡家溢 on 2018/7/22.
+//  Copyright © 2018年 TenPointHelfGame. All rights reserved.
+//
+
+import Foundation
+
+class GameModel {
+    
+    //首先 10點半 需要莊家 玩家 派牌 計分 牌發完
+    //先做一個方法 派牌
+    
+    func 派牌() -> Int {
+        return 0
+    }
+    
+    
+    
+    
+    
+    
+}
--- TenPointHelfGameTests/TenPointHelfGameTests.swift
+++ TenPointHelfGameTests/TenPointHelfGameTests.swift
@@ -11,6 +11,8 @@ import XCTest
 
 class TenPointHelfGameTests: XCTestCase {
     
+    let 遊戲規則 = GameModel()
+    
     override func setUp() {
         super.setUp()
         // Put setup code here. This method is called before the invocation of each test method in the class.
@@ -24,6 +26,29 @@ class TenPointHelfGameTests: XCTestCase {
     func testExample() {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    // 測試 派牌
+    func test派牌() {
+        
+        //基本上 拿到的牌一定是1~13之間 所以
+        let 牌的範圍 = 1 ... 13
+        
+        let 取得牌 = 遊戲規則.派牌()
+        
+        // 糟糕 不知道怎麼檢查數值是否在範圍內
+        // 怎麼測試也忘記了 馬上餵狗 請求支援
+        
+        let 是否在範圍內: Bool
+        switch 取得牌 {
+        case 牌的範圍:
+            是否在範圍內 = true
+        default:
+            是否在範圍內 = false
+        }
+        XCTAssert(是否在範圍內)
+        // 開始第一次測試
+        // 應為我派牌 一定是0 不在我設定的範圍內 所以測試失敗 就先用版本控制 git 記住這次錯誤的測試
     }
     
     func testPerformanceExample() {
